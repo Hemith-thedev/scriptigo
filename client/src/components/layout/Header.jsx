e39cb81365403023f567e3b91da8e6ecc2885e78
hemith-j <hemithj@gmail.com>
2026-05-18 21:18:49 +0530

    feat: add Header component for layout structure and created favicons and logos in .png and .svg types

---
+++ client/src/components/layout/Header.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+const Header = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="" />
+    </>
+  )
+}
+
+export default Header;
